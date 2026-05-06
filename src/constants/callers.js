596697965467e5c9d7b9786424574f1e684cdd1c
Giheon Jeon <jgh030814@naver.com>
2026-05-06 15:24:37 +0900

    ⚡ Perf: 신규 프로필 이미지를 PNG에서 JPG 포맷으로 변환하여 용량 및 로딩 성능 최적화

--- src/constants/callers.js
+++ src/constants/callers.js
@@ -5,15 +5,15 @@ export const CALLERS = {
     { name: '내 사랑 ❤️', audio: `${BASE}audio/male_1.mp3`, image: `${BASE}images/male_1.jpg` },
     { name: '오빠',        audio: `${BASE}audio/male_2.mp3`, image: `${BASE}images/male_2.jpg` },
     { name: '민수',        audio: `${BASE}audio/male_3.mp3`, image: `${BASE}images/male_3.jpg` },
-    { name: '서준이',      audio: `${BASE}audio/male_4.mp3`, image: `${BASE}images/male_4.png` },
-    { name: '도윤이',      audio: `${BASE}audio/male_5.mp3`, image: `${BASE}images/male_5.png` },
+    { name: '서준이',      audio: `${BASE}audio/male_4.mp3`, image: `${BASE}images/male_4.jpg` },
+    { name: '도윤이',      audio: `${BASE}audio/male_5.mp3`, image: `${BASE}images/male_5.jpg` },
   ],
   male: [
     { name: '지연이',         audio: `${BASE}audio/female_1.mp3`, image: `${BASE}images/female_1.jpg` },
     { name: '수진이',         audio: `${BASE}audio/female_2.mp3`, image: `${BASE}images/female_2.jpg` },
     { name: '우리 공주님 👸', audio: `${BASE}audio/female_3.mp3`, image: `${BASE}images/female_3.jpg` },
-    { name: '하은이',         audio: `${BASE}audio/female_4.mp3`, image: `${BASE}images/female_4.png` },
-    { name: '서연이',         audio: `${BASE}audio/female_5.mp3`, image: `${BASE}images/female_5.png` },
+    { name: '하은이',         audio: `${BASE}audio/female_4.mp3`, image: `${BASE}images/female_4.jpg` },
+    { name: '서연이',         audio: `${BASE}audio/female_5.mp3`, image: `${BASE}images/female_5.jpg` },
   ],
 };
 
